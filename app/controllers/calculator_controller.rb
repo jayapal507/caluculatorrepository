b05099670ae4c0be02003aedb3f2c0263f693706
jayapal507 <jayapal.108@gmail.com>
2016-03-30 13:19:29 +0530

    first commit

---
+++ app/controllers/calculator_controller.rb
@@ -0,0 +1,37 @@
+class CalculatorController < ApplicationController
+  def addition
+    
+    @a=params[:a]
+    @b=params[:b]
+    @c=@a.to_i+@b.to_i
+    
+    
+  end
+
+  def subtraction
+    @a=params[:a]
+    @b=params[:b]
+    @c=@a.to_i-@b.to_i
+    
+    
+  end
+
+  def multiplication
+    
+    @a=params[:a]
+    @b=params[:b]
+    @c=@a.to_i*@b.to_i
+    
+  end
+
+  def division
+    @a=params[:a]
+    @b=params[:b]
+    @c=@a.to_f/@b.to_f
+    
+    
+    
+  end
+  
+  
+end
